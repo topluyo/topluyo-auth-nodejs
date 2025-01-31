8c557b7d90c5372254a5f623d0d8037a8d6597c5
Hasan Delibaş <hasan.delibas94@gmail.com>
2025-01-31 04:52:53 +0300

    v2

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const PORT = process.env.PORT || 5002
 const http = require('http');
 const https = require("https")
 const express = require('express')
+const cors = require('cors');
 const app = express()
 const session = require('express-session');
+const path = require('path');
 const server = http.Server(app);
 
+app.use(cors({
+    origin: 'https://topluyo.com',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(session({
   secret: 'your-secret-key',
@@ -13,7 +20,8 @@ app.use(session({
   saveUninitialized: false
 }));
 
-app.get("/", function(req, res){
+
+app.get("/path-to-connection/", function(req, res){
     if(req.query.token){
         https.get("https://api.topluyo.com/user/token/"+req.query.token, (response) => {
             let data = '';
@@ -23,20 +31,18 @@ app.get("/", function(req, res){
                     let response = JSON.parse(data)
                     if(response.status=="success"){
                         req.session.user = response.data;
-                        res.json(req.session.user);
+                        res.json(response.data);
                     }
                 }catch(e){
-                    res.json("Oturum Açılamadı");
+                    res.json("Oturum Açılamadı" + e.message);
                 }
             }); 
         }); 
     }else{
         res.json("Oturum Açılamadı, Token Gelmedi");
     }
-})
+});
 
-
-
-server.listen(PORT, e => {
-  console.log("Sunucu Çalıştı", PORT)
-})
+server.listen(PORT, () => {
+    console.log("Sunucu Çalıştı", PORT)
+}); 
